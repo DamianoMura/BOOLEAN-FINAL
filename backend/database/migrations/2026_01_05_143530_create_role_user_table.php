d5040d958b83817918463d289c66b5b3eed2efd3
DamianoMura <mura.damiano14@gmail.com>
2026-01-05 15:37:06 +0100

    [upd] created migrations for relations and seeding first 2 roles as default and default admin user

--- backend/database/migrations/2026_01_05_143530_create_role_user_table.php
+++ backend/database/migrations/2026_01_05_143530_create_role_user_table.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('roles_users', function (Blueprint $table) {
+        Schema::create('role_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('role_id')->constrained()->onDelete('cascade');
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles_users');
+        Schema::dropIfExists('role_user');
     }
 };
--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call(RoleSeeder::class);
+        User::create([
+            'name' => 'Demianz',
+            'email' => 'demianz@jdwdev.it',
+            'password' => bcrypt('asdasdasd'),
         ]);
+        User::first()->roles()->attach(Role::where('name', 'admin')->first());
     }
 }
