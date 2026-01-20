146a006c6a1b07f654a6143854e8c63799ab490c
DamianoMura <mura.damiano14@gmail.com>
2026-01-20 17:28:47 +0100

    [upd] edited bootstrao original navbar after implementing react-bootstrap

--- frontend/src/layouts/components/Navbar.jsx
+++ frontend/src/layouts/components/Navbar.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { Navbar, Nav,  Container } from 'react-bootstrap';
+import * as Dom from 'react-bootstrap';
 
 const CustomNavbar = () => {
   return (
-    <Navbar bg="light" expand="lg" className="px-3">
-      <Container fluid>
-        <Navbar.Brand as={Link} to="/home">Navbar</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="my-2 me-auto my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-            
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
-            
-            
-          </Nav>
-          
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Dom.Navbar bg="light" expand="lg" className="px-3">
+      <Dom.Container fluid>
+        <Dom.Navbar.Brand as={Link} to="/home">Navbar</Dom.Navbar.Brand>
+        <Dom.Navbar.Toggle aria-controls="navbarScroll" />
+        <Dom.Navbar.Collapse id="navbarScroll">
+          <Dom.Nav className="my-2 me-auto my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+            <Dom.Nav.Link as={Link} to="/about">About</Dom.Nav.Link>
+            <Dom.Nav.Link as={Link} to="/projects">Projects</Dom.Nav.Link>
+          </Dom.Nav>
+        </Dom.Navbar.Collapse>
+      </Dom.Container>
+    </Dom.Navbar>
   );
 }
 
