bb32e3c79ebd582ce716b4cb98dc58cb114db7c3
DamianoMura <mura.damiano14@gmail.com>
2026-01-21 03:04:00 +0100

    [add] axios calls implementing FiltersContext

--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -1,25 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useNavigate, useLocation } from "react-router-dom";
 import { useFilters } from '../context/FiltersContext';
 import Filters from './components/projects/Filters';
 
 import axios from 'axios';
 
 const ProjectsPage = () => {
+  const navigate = useNavigate();
   const { filters } = useFilters(); // filters from context
-  const [projects,setProjects]=useState([]);
-  const [loadingProjects,setLoadingProjects]=useState();
+  const [projects,setProjects]=useState([]);//state for projects
+  const [loadingProjects,setLoadingProjects]=useState(); //loading state from axios call
 
-  // Fetch categories
+// we make a db call with location.search as string query
+	  useEffect(()=>{
+    axios.get(`http://localhost:8000/api/projects`).then((resp)=>{
+      
+      setProjects(resp.data.data);
+    }).catch((err) => navigate("/not-found", {replace : true}));
+  },[])
+  useEffect(()=>{
+		const qS = new URLSearchParams();
+      const currentFilters=Object.entries(filters)
+      console.log(currentFilters)
+      currentFilters.map((query)=>{
+        qS.set(query[0],query[1])
+      })
+			navigate({ search: qS.toString() }, { replace: true });
+	},[filters])
 
 
 
   return (
-    <div className="container py-4">
+    <div className="p-5 bg-white rounded-lg shadow">
       
       
-      <Filters
-       
-      />
+      <Filters />
       
     
     </div>
